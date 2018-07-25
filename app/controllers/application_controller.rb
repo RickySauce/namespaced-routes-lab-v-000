2a4adbcadd3a845ad4c1980fa7a62af7c81373fd
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-25 18:29:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-
-  def set_preferences
-    @prefences = Preference.first
-  end
-
 end
